f4b11dcde1d8105de18a257d1f894325e3cc99ac
serg last <sergeniy9@gmail.com>
2026-06-03 12:42:18 +0300

    Add Vercel PHP entrypoint

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 ﻿<?php
+declare(strict_types=1);
+
+/*
+  Vercel запускает PHP из /api.
+  Мы переносим рабочую директорию в корень проекта
+  и подключаем обычный index.php сайта.
+*/
 
 chdir(dirname(__DIR__));
 
